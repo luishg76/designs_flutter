551f978207561d7a4fff167649b853896790dfa0
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-09-08 16:12:54 -0500

    IniciandoRepo(08-09-022)Laptop

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+        body: Column(
+          children: [
+            Container(child: Image(image:)),
+            Center(
+             child: Text('Hola Mundo'),
+       ),
+          ],
+        ),
+    );
+  }
+}
