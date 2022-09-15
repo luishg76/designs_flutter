d1c83b9781e73fc1fe243a01fcbc72cd4d887db5
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-09-15 18:50:14 -0500

    salva(15-09-022)laptop

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,18 +26,93 @@ class _BodyHome extends StatelessWidget {
     return SafeArea(
       bottom: false,
       child: SingleChildScrollView(
-           child: Container(
-             padding: EdgeInsets.only(left: 30.0, top: 50.0, right: 15.0),                      
-             child: Column(
+           child:Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [              
-                Text('Classify Transaction', style: TextStyle(color: Colors.white, fontSize: 25, ),),
-                SizedBox(height: 10,),
-                Text('Classify this transaction into a particular category', style: TextStyle(color: Colors.white30, fontSize: 18, ))
+              children: [       
+                _titleWidget(), 
+                _TableWidget(),
               ],
              ),
            ),
-      ),
-    );
+      );    
   }
 } 
+
+class _TableWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+     return Container(
+        padding: EdgeInsets.only(top: 25, left: 10,right: 10),
+        child: Table(
+          children: [
+            TableRow(
+              children:[_SingleCardWidget(title: 'General',icono: Icons.widgets_outlined, color:Colors.blue), 
+                        _SingleCardWidget(title: 'Transport',icono: Icons.train, color:Colors.purple)]
+            ),           
+            TableRow(
+              children:[_SingleCardWidget(title: 'Shopping',icono: Icons.shopping_bag_outlined, color:Colors.pink), 
+                        _SingleCardWidget(title: 'Bills',icono: Icons.clear_all_outlined, color:Colors.purple)]
+            ),
+            TableRow(
+              children:[_SingleCardWidget(title: 'Entertaiment',icono: Icons.movie_outlined, color:Colors.blue), 
+                        _SingleCardWidget(title: 'Grocery',icono: Icons.train, color:Colors.purple)]
+            ),
+          ]
+          ),
+       );
+  }
+}
+
+class _SingleCardWidget extends StatelessWidget {
+  final Color color;
+  final IconData icono;
+  final String title;
+  _SingleCardWidget({required this.title, required this.icono, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+     return Container(
+        margin: EdgeInsets.all(10),
+        height: 170,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color.fromRGBO(62, 66, 107, 0.7)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container( 
+              //height: 60,
+              //width: 60,
+              padding: EdgeInsets.all(15),
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60), 
+                gradient: RadialGradient(colors:[Color.fromRGBO(color.red+10, color.green+10, color.blue+10, 1), color ])
+                ),
+              child:Icon(icono,color:Colors.white60, size: 35,)
+              ),
+            //SizedBox(height: 25),
+            Text(title, style: TextStyle(color: color, ),)
+          ],
+
+        ),
+       );
+  }
+}
+
+class _titleWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+     return Container(
+             padding: EdgeInsets.only(left: 30.0, top: 25.0, right: 30.0),
+             child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Text('Classify Transaction', style: TextStyle(color: Colors.white, fontSize: 25, ),),
+                     SizedBox(height: 10,),
+                     Text('Classify this transaction into a particular category', style: TextStyle(color: Colors.white30, fontSize: 18, ))
+        ],
+       )
+      );
+  }
+}
--- lib/widgets/custom_botton_navigation.dart
+++ lib/widgets/custom_botton_navigation.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
 
-class CustomBottonNavigation extends StatelessWidget {
+class CustomBottonNavigation extends StatefulWidget {
+
+  @override
+  State<CustomBottonNavigation> createState() => _CustomBottonNavigationState();
+}
+
+class _CustomBottonNavigationState extends State<CustomBottonNavigation> {
+  int _selectIndex=0;
 
   @override
   Widget build(BuildContext context) {
      return BottomNavigationBar(
       selectedItemColor: Colors.pink,
+      //showSelectedLabels: false,
       unselectedItemColor: Color.fromRGBO(116,117,152,1),
-      backgroundColor: Color.fromRGBO(55, 57, 84, 1),
-      showSelectedLabels: false,
       showUnselectedLabels: false,
-      //currentIndex: 0,
+      backgroundColor: Color.fromRGBO(55, 57, 84, 1),
+      currentIndex: _selectIndex,
       items: [
         BottomNavigationBarItem(
           icon: Icon(Icons.calendar_today_outlined),
@@ -21,12 +28,19 @@ class CustomBottonNavigation extends StatelessWidget {
           icon: Icon(Icons.pie_chart_outline_outlined),
           label: 'Grafico'
           ),
-          BottomNavigationBarItem(
+        BottomNavigationBarItem(
           icon: Icon(Icons.supervised_user_circle_outlined),
           label: 'Usuarios'
           ) 
       ],
 
-      );       
+      onTap: (index){
+        setState(() {
+          _selectIndex=index;
+        });                         
+      },
+
+      );
+
   }
 }
