5c58ede08f18d5ac4114cd8131991dc2c7d8d512
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-09-14 20:55:06 -0500

    salvapc-14-09-022

--- lib/widgets/custom_botton_navigation.dart
+++ lib/widgets/custom_botton_navigation.dart
@@ -6,10 +6,26 @@ class CustomBottonNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return BottomNavigationBar(
-      items:[
-          BottomNavigationBarItem(icon:Icon(Icons.calendar_today_outlined)),
-          BottomNavigationBarItem(icon:Icon(Icons.calendar_today_outlined))
-          ],
+      selectedItemColor: Colors.pink,
+      unselectedItemColor: Color.fromRGBO(116,117,152,1),
+      backgroundColor: Color.fromRGBO(55, 57, 84, 1),
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      //currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today_outlined),
+          label: 'Calendario'
+          ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.pie_chart_outline_outlined),
+          label: 'Grafico'
+          ),
+          BottomNavigationBarItem(
+          icon: Icon(Icons.supervised_user_circle_outlined),
+          label: 'Usuarios'
+          ) 
+      ],
 
       );       
   }
